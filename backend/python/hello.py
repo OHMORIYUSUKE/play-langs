e405e8717f1ddf6e2256459fbb3fd144ab080d2f
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2022-01-02 02:09:12 +0900

    パーミッションを有効化

--- backend/python/hello.py
+++ backend/python/hello.py
@@ -1,9 +1,1 @@
-from urllib import request
-
-for i in range(2):
-    response = request.urlopen('https://zenn.dev/u_tan/articles/f351dc7e84e6f3')
-    content = response.read()
-    response.close()
-    html = content.decode()
-    title = html.split('<title>')[1].split('</title>')[0]
-    print(title)
+print(100)
--- backend/python/shell.sh
+++ backend/python/shell.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo bash hello.sh
+sh hello.sh
